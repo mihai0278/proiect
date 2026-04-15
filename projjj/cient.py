e0c70fc5f93a886263816c917b0acb1e6aef49b2
mihai0278 <165584082+mihai0278@users.noreply.github.com>
2026-04-15 18:40:56 +0300

    Add files via upload

---
+++ projjj/cient.py
@@ -0,0 +1,74 @@
+import socket
+import ssl
+from typing import Final
+import time
+from datetime import datetime 
+
+
+PORT_TELECOMANDA: Final[int] = 5005
+TIMEOUT: Final[int] = 5
+ADRESA_TELECOMANDA: Final[str] = 'proiectt.sbs'
+#ADRESA_TELECOMANDA: Final[str] = '192.168.0.93'
+
+
+context = ssl.create_default_context()
+telecomanda_nesecurizata = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+telecomanda_nesecurizata.settimeout(TIMEOUT)
+telecomanda = context.wrap_socket(telecomanda_nesecurizata, server_hostname='proiectt.sbs')
+print("telecomanda creata, asteapta conexiune...")
+
+try:
+    print(f"incerc conexiune la {ADRESA_TELECOMANDA}:{PORT_TELECOMANDA}...")
+    telecomanda.connect((ADRESA_TELECOMANDA, PORT_TELECOMANDA))
+    print("conexiune stabilita cu telecomanda")
+    mesaj="Lumina: OFF, Temperatura: 22C, Alarma: ON"
+    telecomanda.send(mesaj.encode())
+    print("telecomanda trimisa")
+
+
+except socket.timeout:
+    print("❌ Timeout (host unreachable / firewall drop)")
+
+except ConnectionRefusedError:
+    print("❌ Connection refused (port closed)")
+
+except socket.gaierror:
+    print("❌ DNS resolution failed")
+
+except OSError as e:
+    print(f"❌ OS error: {e}")
+
+finally:
+    telecomanda.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ projjj/test.py
@@ -0,0 +1,55 @@
+import socket
+import sqlite3
+import ssl
+
+try :
+    db = sqlite3.connect('/opt/server-py/comunicatii.db')
+    cursor = db.cursor()
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(
+    certfile = "/etc/letsencrypt/live/proiectt.sbs/fullchain.pem", 
+    keyfile = "/etc/letsencrypt/live/proiectt.sbs/privkey.pem"
+)
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0',5005))
+    server.listen(1)
+    print("server pornit,asteapta conectiuni...")
+except sqlite3.Error as e :
+    print(f"❌ Database error: {e}")
+except Exception as e :
+    print(f"❌ Server error: {e}")
+
+
+
+while True :
+    try :
+        fir_neprotejat, adresa = server.accept()
+        print(f"conexiune de la {adresa}")
+        with context.wrap_socket(fir_neprotejat, server_side=True) as fir_protejat:
+            mesaj_total = ""
+            while True:
+                date_raw = fir_protejat.recv(4)
+                if not date_raw:
+                     break
+                mesaj_total += date_raw.decode()
+                
+
+                if not mesaj_total:
+                    print("❌ No data received")
+                    continue
+
+            cursor.execute("""
+                    INSERT INTO loguri (expeditor, mesaj, timestamp) 
+                    VALUES (?, ?, datetime('now', 'localtime'))
+                """, (adresa[0], mesaj_total))
+            db.commit()
+            print(f"comanda primita: {mesaj_total}")
+
+    except sqlite3.Error as e:
+        print(f"❌ Database error: {e}")
+    except Exception as e:
+        print(f"❌ Client error: {e}")
+    finally:
+        fir_neprotejat.close()
